dae85112f4663a16e5a90415d2a8beabcce5b6f2
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-02-22 17:27:37 -0600

    deploying

---
+++ client/build/precache-manifest.d3982fcb7d4d0efc6bdd98043af973c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b90ab791bad26f9a6fc5",
+    "url": "/static/js/main.b90ab791.chunk.js"
+  },
+  {
+    "revision": "50a9deda5979e8bbc88b",
+    "url": "/static/js/2.50a9deda.chunk.js"
+  },
+  {
+    "revision": "b90ab791bad26f9a6fc5",
+    "url": "/static/css/main.fd0ed444.chunk.css"
+  },
+  {
+    "revision": "8417e87a2f54f6ac6b5da1dc9c17cf3b",
+    "url": "/index.html"
+  }
+];
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,9 +10,9 @@ class NavBar extends Component {
                 <div class="nav-wrapper">
                     <a href="#!" class="brand-logo">BookKeeper</a>
                     <ul class="right hide-on-med-and-down">
-                        <li><a href="/search"><i class="material-icons left">search</i></a></li>
-                        <li><a href="/"><i class="material-icons left">view_module</i></a></li>
-                        <li><a href="https://github.com/rehlingera/Google-Books-Search"><i class="material-icons left">code</i></a></li>
+                        <li><a title="Search for Books" href="/search"><i class="material-icons left">search</i></a></li>
+                        <li><a title="View Library" href="/"><i class="material-icons left">view_module</i></a></li>
+                        <li><a title="See the Code" href="https://github.com/rehlingera/Google-Books-Search"><i class="material-icons left">code</i></a></li>
                     </ul>
                 </div>
             </nav>
