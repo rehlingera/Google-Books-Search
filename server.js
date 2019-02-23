d612ec9c64329dd74a515cdba7383a34991fd850
Relliner <41902228+rehlingera@users.noreply.github.com>
2019-02-22 21:40:02 -0600

    deploying

--- server.js
+++ server.js
@@ -5,22 +5,13 @@ const app = express();
 const mongoose = require("mongoose");
 const routes = require("./routes")
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
 app.use(routes);
-
-// Send every other request to the React app
-// Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
-// });
 
 MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
